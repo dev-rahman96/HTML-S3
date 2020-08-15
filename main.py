242e030bbe4e44d650d5eb8a196d7f9d44e72054
dev.rahman96 <dev.rahman96@gmail.com>
2020-08-15 12:47:25 -0400

    init the message and imported web browser

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import webbrowser
 class HtmlDocument:
     def __init__(self):
         pass
@@ -6,23 +7,16 @@ class HtmlDocument:
 
 class HtmlManager:
     def __init__(self):
-        pass
+        self.message = """<html><head></head>
+        <body><p>Hello World Test!</p></body>
+        </html>"""
         
     def writehtml(self):
         samplehtml = open('helloworld.html','w')
-        
-        message = """<html><head></head>
-        <body><p>Hello World!</p></body>
-        </html>"""
-        
-        samplehtml.write(message)
+        samplehtml.write(self.message)
         samplehtml.close()
+        webbrowser.open_new_tab('helloworld.html')
         
-        
-        
-            
-    
-
 
 class AWSManager:
     def __init__(self):
@@ -31,6 +25,7 @@ class AWSManager:
 
 s1 = HtmlManager()
 s1.writehtml()  
+
 
 
                  
